95ecf6270c7ceb5d83286f85d1a487e1a0cdcfb9
Mbuco <mbuco2019@gmail.com>
2022-12-23 17:42:53 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="css/style.css">
+ <!-- Corpo <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet"> 
+-->
+   <title>Design Flexbox </title>
+
+
+</head>
+<body>
+    <header>
+        <a href=""> <img src="img/32.jpg"  alt="Viva la vida>"</a>
+        
+        <nav> 
+               
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Reservas</a></li>
+                    <li><a href="">Contactos</a></li>
+             
+        </nav>
+     
+    </header>
+     <!-- Corpo -->
+    <section class="hero">
+        <h1> Venha conhecer o paraiso</h1>
+        <h3>Conheça o conforto </h3>
+        <a class="btn" href=""> Saiba mais</a>
+    </section>
+    <section class ="conheca">
+        <h3> Conheça o Hotel</h3>
+        <p> olá! Saiba mais sobre o nosso hotem e desfrute de tudo que estiver a 
+            sua disposiçãoolá! Saiba mais sobre o nosso hotem e desfrute de tudo que
+             estiver a sua disposiçãoolá! Saiba mais sobre o nosso hotem e desfrute de
+              tudo que estiver a sua disposiçãoolá! Saiba mais sobre o nosso hotem e desfrute 
+              de tudo que estiver a sua disposiçãoolá! Saiba mais sobre o nosso hotem 
+            e desfrute de tudo que estiver a sua disposição</p>
+        <hr>
+        <ul class="grid">
+            <li class="small" style="background-image: url(img/25.jpg);"></li>
+            <li class="large" style="background-image: url(img/21.jpg);"></li>
+            <li class="large" style="background-image: url(img/32.jpg);"></li>
+            <li class="small" style="background-image: url(img/3.jpg);"></li>
+        </ul>
+    </section>
+    <section class="caracteristicas">
+    <h3> O melhor hotem do mundo</h3>
+        <p> olá! blalalalalalalalalalalalalalfhdhgfwywfjfhjhwbkjfhfhfj ewfb 
+            </p>
+        <hr>
+        <ul class="grid">
+            <li> <i class="fas fa-location-arrow"></i> 
+            <h3> Localização</h3>
+        <p> estamos ocalizados a sul da estalagem junto ao mar vermelho do egito próximo ao estádio do maracana 
+            </p>  
+        
+        </li>
+
+        <li> <i class="fas fa-gift"></i> 
+            <h3> Pacotes de ofertas</h3>
+        <p> Encontrarás tudo que tens procurado em sua vida! 
+            </p>  
+        
+        </li>
+
+        <li> <i class="fas fa-home"></i> 
+            <h3> Hospitalidade</h3>
+        <p> Alem da sua cama, também encontrarás cama para o seu rato, para não falar dos seus 
+            </p>  
+        
+        </li>
+            <li class="large" style="background-image: url(img/21.jpg);"></li>
+            <li class="large" style="background-image: url(img/32.jpg);"></li>
+            <li class="small" style="background-image: url(img/3.jpg);"></li>
+        </ul>
+    </section>
+   <section class="newslatter">
+   <h3> Informações Adicionais</h3>
+        <p> Deixe seu email e receba as últimas novidades! 
+            </p> 
+            <hr>
+            <form>
+                <input type="emai" placeholder="Email">
+                <button type="submit" > Cadastrar </button>
+            
+            </form>
+   </section>
+<footer>
+    <ul>
+        <li> <a href=""><i class="fab fa-facebook"></i></a></li>
+        <li> <a href=""><i class="fab fa-twitter"></i></a></li>
+        <li> <a href=""><i class="fab fa-pinterest"></i></a></li>
+
+    </ul>
+    <p> Todos direitos reservado 2022</p>
+</footer>
+
+        
+</body>
+</html>
